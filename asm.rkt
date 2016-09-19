15c9e7a8c0f2066b177479758d59c160edc898ec
Dustin Jamner <dijamner@me.com>
2016-09-19 17:05:25 -0400

    push list operations upwards.

--- asm.rkt
+++ asm.rkt
@@ -19,7 +19,7 @@
   [((list label ': )) #:when (symbol? label) (string-append (symbol->string label) ":\n")]
   ;; works for both instructions and directives
   [((cons instr args)) (string-append (symbol->string instr) " "
-                                 (print-instr-args args) "\n")]
+                                 (print-instr-args (first args) (rest args)) "\n")]
   [(_) (error "Instruction or directive not supported: " instr)])
 
 (define (token->string tok)
@@ -30,7 +30,7 @@
 (define (token? arg)
   (or (symbol? arg) (number? arg) (string? arg)))
 
-(define (print-instr-args args)
+(define (print-instr-args first-arg rest-args)
   (string-join (map (λ (arg)
                       (match arg
                         [(list n reg) #:when (and (number? n) (symbol? reg))
@@ -42,8 +42,8 @@
                         [_ #:when (token? arg)
                            (string-append " " (token->string arg))]
                         [_ (error "Failed to match: " arg)]))
-                    (rest args))
+                    rest-args)
                " " ;;TODO: fix commas
-               #:before-first (token->string (first args))))
+               #:before-first (token->string first-arg)))
 
 
