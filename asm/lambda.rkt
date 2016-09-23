b0a4aea297a71debd826bbdc7f9f2d7828898087
AlexKnauth <alexander@knauth.org>
2016-09-23 00:22:28 -0400

    translate syntax-case to syntax-parse

--- asm/lambda.rkt
+++ asm/lambda.rkt
@@ -1,28 +1,29 @@
 #lang racket
-(require (rename-in "lifted-lambda.rkt"
+(require (for-syntax syntax/parse)
+         (rename-in "lifted-lambda.rkt"
                    [prog lifted-prog]))
 
 (provide prog)
 
 (define-syntax (prog stx)
-  (syntax-case stx ()
+  (syntax-parse stx
     [(prog expr)
-     (let-values [((main blocks) (lift-lambdas #'expr))]
-     #`(lifted-prog #,main . #,blocks))]))
+     (define-values [main blocks] (lift-lambdas #'expr))
+     #`(lifted-prog #,main . #,blocks)]))
 
 (define-for-syntax (lift-lambdas expr)
-  (syntax-case expr (lambda print)
+  (syntax-parse expr #:literals (lambda print)
     [(lambda body)
-     (let-values [((body-expr body-blocks) (lift-lambdas #'body))]
-       (with-syntax [(lambda-name (car (generate-temporaries '(lambda))))]
-         (values #'lambda-name #`((lambda-name #,body-expr) . #,body-blocks))))]
+     #:do [(define-values [body-expr body-blocks] (lift-lambdas #'body))]
+     #:with lambda-name (car (generate-temporaries '(lambda)))
+     (values #'lambda-name #`((lambda-name #,body-expr) . #,body-blocks))]
     [(fun arg) ;;TODO: never reached
-     (let-values [((fun-expr fun-blocks) (lift-lambdas #'fun))
-                  ((arg-expr arg-blocks) (lift-lambdas #'arg))]
-       (with-syntax [((arg-block ...) arg-blocks)
-                     ((fun-block ...) fun-blocks)]
-         (values #`(#,fun-expr #,arg-expr)
-                 #'(arg-block ... fun-block ...))))]
-    [n (exact-nonnegative-integer? (syntax->datum #'n)) (values #'n #'())]
-    [str (string? (syntax->datum #'str)) (values #'str #'())]
+     #:do [(define-values [fun-expr fun-blocks] (lift-lambdas #'fun))
+           (define-values [arg-expr arg-blocks] (lift-lambdas #'arg))]
+     #:with (arg-block ...) arg-blocks
+     #:with (fun-block ...) fun-blocks
+     (values #`(#,fun-expr #,arg-expr)
+             #'(arg-block ... fun-block ...))]
+    [n:nat (values #'n #'())]
+    [str:str (values #'str #'())]
     [print (values #'print #'())]))
