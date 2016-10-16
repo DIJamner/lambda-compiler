0bb86bf29bfaa25895790493d43813c44b939302
Dustin Jamner <dijamner@me.com>
2016-10-16 12:31:31 -0400

    Restructure build output to clean up temporary files but leave final executable.

--- x64/lib/build.sh
+++ x64/lib/build.sh
@@ -2,17 +2,19 @@
 alias nasm2=/usr/local/bin/nasm
 #TODO: grab filename only
 mkdir -p "$2"
+mkdir "$2/tmp"
 #nasm -f macho64 gc.s -o "$2/gc.o"
 pushd gc
 cargo rustc -- --crate-type=staticlib
-cp target/debug/deps/libgc.a "../$2/libgc.a"
+cp target/debug/deps/libgc.a "../$2/tmp/libgc.a"
 popd
-nasm -f macho64 lib.s -o "$2/lib.o"
-nasm -f macho64 "$1" -o "$2/$1.o"
-pushd "$2"
+nasm -f macho64 lib.s -o "$2/tmp/lib.o"
+nasm -f macho64 "$1" -o "$2/tmp/$1.o"
+pushd "$2/tmp"
 #TODO: switch off of debug
-ld -L . -no_pie -demangle -dynamic -arch x86_64 -macosx_version_min 10.11.0 -o main lib.o "$1.o" -lc -lgc -lSystem -lm
+ld -L . -no_pie -demangle -dynamic -arch x86_64 -macosx_version_min 10.11.0 -o ../main lib.o "$1.o" -lc -lgc -lSystem -lm
+cd ..
 ./main
 echo ""
 popd
-#rm -r "$2"
+rm -r "$2/tmp"
