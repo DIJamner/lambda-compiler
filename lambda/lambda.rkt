f134db7983ef22fcdd089e88da8897a3b4f545a8
AlexKnauth <alexander@knauth.org>
2016-10-22 01:53:01 -0400

    allow named variables as arguments instead of DeBruijn indices

--- lambda/lambda.rkt
+++ lambda/lambda.rkt
@@ -1,29 +1,44 @@
 #lang racket
-(require (for-syntax syntax/parse)
-         (rename-in "lifted-lambda.rkt"
-                   [prog lifted-prog]))
+(require (for-syntax racket/match syntax/parse)
+         (rename-in "debruijn-lambda.rkt"
+                    [prog debruijn-prog]))
 
 (provide prog)
 
 (define-syntax (prog stx)
   (syntax-parse stx
     [(prog backend expr)
-     (define-values [main blocks] (lift-lambdas #'expr))
-     #`(lifted-prog backend #,main . #,blocks)]))
+     #:with debruijn-expr (convert-debruijn-indices #'expr empty-ids)
+     #`(debruijn-prog backend debruijn-expr)]))
 
-(define-for-syntax (lift-lambdas expr)
-  (syntax-parse expr #:literals (lambda print)
-    [(lambda body)
-     #:do [(define-values [body-expr body-blocks] (lift-lambdas #'body))]
-     #:with lambda-name (car (generate-temporaries '(lambda)))
-     (values #'lambda-name #`((lambda-name #,body-expr) . #,body-blocks))]
-    [(fun arg)
-     #:do [(define-values [fun-expr fun-blocks] (lift-lambdas #'fun))
-           (define-values [arg-expr arg-blocks] (lift-lambdas #'arg))]
-     #:with (arg-block ...) arg-blocks
-     #:with (fun-block ...) fun-blocks
-     (values #`(#,fun-expr #,arg-expr)
-             #'(arg-block ... fun-block ...))]
-    [n:nat (values #'n #'())]
-    [str:str (values #'str #'())]
-    [print (values #'print #'())]))
+(begin-for-syntax
+  ;; An IdEnv is one of:
+  ;; - empty-ids
+  ;; - (extend-ids Id IdEnv)
+  (define empty-ids '())
+  (define extend-ids cons)
+
+  ;; find-index : IdEnv Id Natural -> Natural
+  (define (find-index ids x i)
+    (match ids
+      ['() (raise-syntax-error #f "unbound identifier" x)]
+      [(cons id ids)
+       (if (bound-identifier=? id x)
+           i
+           (find-index ids x (add1 i)))]))
+  
+  ;; convert-debruijn-indices : Stx IdEnv -> Stx
+  (define (convert-debruijn-indices stx ids)
+    (syntax-parse stx #:literals (lambda print)
+      [print #'print]
+      [x:id (find-index ids #'x 0)]
+      [(lambda (x:id) body:expr)
+       #:with debruijn-body (convert-debruijn-indices #'body (extend-ids #'x ids))
+       #'(lambda debruijn-body)]
+      [(fun:expr arg:expr)
+       #:with debruijn-fun (convert-debruijn-indices #'fun ids)
+       #:with debruijn-arg (convert-debruijn-indices #'arg ids)
+       #'(debruijn-fun debruijn-arg)]
+      [n:nat #'n]
+      [str:str #'str]
+      )))
