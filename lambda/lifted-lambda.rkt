a0d6b303b4534df402358904f6a53c708420887f
Dustin Jamner <dijamner@me.com>
2016-12-07 22:42:05 -0500

    Initial implementation.

--- lambda/lifted-lambda.rkt
+++ lambda/lifted-lambda.rkt
@@ -103,15 +103,17 @@
 ;; peforms an exhaustive search to unify functions with identical bodies
 ;; and remove dead code
 ;; TODO: does not handle recursive functions
+;; TODO: does not replace function calls in main
 (define-syntax (unify-functions stx)
   (syntax-parse stx
     [(unify-functions backend #:lifted-opt (opt-rest ...) start-exp (name fun) ...)
      #:do [(define-values (used-by uses) (used-funs #'start-exp #'((name fun) ...)))
            (define simplify-list (dedup-ordered-list uses))
            (define new-function-map (foldl simplify-if-possible
-                                           (fun-hash #'((name fun) ...)) simplify-list))]
+                                           (fun-hash #'start-exp #'((name fun) ...)) simplify-list))]
      #:with ((uname ufun) ...) (map (lambda (p) (list (car p) (cdr p)))
-                                    (hash->list new-function-map))
+                                    (hash->list (hash-remove new-function-map '_main)))
+     #:with new-start-exp (hash-ref new-function-map '_main)
      #'(prog backend #:lifted-opt (opt-rest ...) start-exp (uname ufun) ...)]))
 
 (define-for-syntax (simplify-if-possible fun fun-map)
@@ -135,7 +137,7 @@
      #'(sub-fun sub-arg)]
     [n:nat #'n]
     [s:str #'s]
-    [v:id #:when (equal? var #'v)
+    [v:id #:when (equal? var (syntax->datum #'v))
           replace-exp]
     [v:id #'v]))
 
@@ -143,7 +145,10 @@
 (define-for-syntax (find-equal-function fun-map fun)
   (define fun-body (hash-ref fun-map fun))
   (foldl (lambda (k res)
-           (if (and (not res) (equal? fun-body (hash-ref k)))
+           (if (and (not res)
+                    (not (equal? k fun))
+                    (equal? (syntax->datum fun-body)
+                            (syntax->datum (hash-ref fun-map k))))
                k
                res))
          #f
@@ -154,7 +159,9 @@
 (define-for-syntax (dedup-ordered-list uses)
   (cond [(hash-empty? uses) (values (list) uses)]
         [else
-         (define assoc-list (hash->list uses))
+         (define assoc-list (map (lambda (p)
+                                   (cons (car p) (set->list (cdr p))))
+                                 (hash->list uses)))
          (define graph (unweighted-graph/adj assoc-list))
          (define-values (discover pred finish) (dfs graph))
          (define vertex-list (hash-keys uses))
@@ -165,10 +172,10 @@
   
 
 ;; compute the ε-closure of the input
-;; TODO: fails to account for loops (loops do no curr. occur, but may in the future)
+;; TODO: fails to account for loops (loops do not curr. occur, but may in the future)
 (define-for-syntax (used-funs exp funs)
-  (define worklist (get-free-vars exp))
-  (define function-map (fun-hash funs))
+  (define worklist (list '_main))
+  (define function-map (fun-hash exp funs))
   (define uses-map (var-hash function-map))
   (define used-by-map (empty-var-hash function-map))
   (used-funs-closure used-by-map uses-map worklist))
@@ -184,41 +191,54 @@
          (define e-used-by (hash-ref used-by-map e))
          ;; get new knowledge of which functions e uses
          (define updated-e-uses
-           (apply free-id-set-union
-                   (free-id-set-map e-uses (lambda (e-use) (hash-ref uses-map e-use)))))
+           (apply set-union
+                   (set-map e-uses (lambda (e-use) (hash-ref uses-map e-use)))))
          ;; find which ones are new
          (define new-e-uses
-           (free-id-set-subtract
+           (set-subtract
             updated-e-uses
             e-uses))
          ;; if we got no new knowledge, we are done with e
          ;; otherwise, update the uses map and used-by map
-         (if (zero? (free-id-set-count new-e-uses))
+         (if (zero? (set-count new-e-uses))
              (used-funs-closure used-by-map uses-map (rest worklist))
              (used-funs-closure (add-use used-by-map new-e-uses e)
                                 (hash-set uses-map e updated-e-uses)
                                 worklist))]))
 
 (define-for-syntax (add-use used-by-map new-e-uses e)
-  (foldl (lambda (map x)
-           (hash-update map x (lambda (s) (free-id-set-add s e))))
+  (foldl (lambda (x map)
+           (hash-update map x (lambda (s) (set-add s e))))
          used-by-map
-         (free-id-set->list new-e-uses)))
+         (set->list new-e-uses)))
 
 
-(define-for-syntax (empty-var-hash fun-hash)
-  (define keys (hash-keys fun-hash))
-  (foldr (lambda (x rst) (cons x (cons '() rst))) keys (list)))
+(define-for-syntax (empty-var-hash fun-map)
+  (for/hash ([(k v) (in-hash fun-map)])
+    (values k (set))))
   
 
 (define-for-syntax (var-hash fun-hash)
   (define keys (hash-keys fun-hash))
   (define key/val-pairs (map (lambda (k) (list k (get-free-vars (hash-ref fun-hash k)))) keys))
-  (apply hash (list->set (apply append key/val-pairs))))
+  (apply hash (apply append key/val-pairs)))
 
-(define-for-syntax (fun-hash stx)
-  (apply hash (apply append (map syntax->list (syntax->list stx)))))
+(define-for-syntax (fun-hash start-exp stx)
+  (hash-set (apply hash (apply append (map (lambda (p)
+                                             (define ls (syntax->list p))
+                                             (cons (syntax->datum (first ls)) (rest ls)))
+                                           (syntax->list stx))))
+            '_main
+            start-exp))
 
+;; TODO handle recursive defs
 (define-for-syntax (get-free-vars stx)
-  (parameterize ([current-namespace (make-empty-namespace)])
-    (free-vars (expand stx))))
+  (syntax-parse stx
+    [(fun:expr arg:expr)
+     (set-union (get-free-vars #'fun)
+                        (get-free-vars #'arg))]
+    [var:id (set (syntax->datum #'var))]
+    [n:nat (set)]
+    [s:str (set)]))
+
+                        
