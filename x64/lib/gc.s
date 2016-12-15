bdbdcb4f6466db5ddaa0bf36ab0957dd1a85eda3
Dustin Jamner <dijamner@me.com>
2016-12-15 17:15:52 -0500

    Remove outdated file.

--- x64/lib/gc.s
+++
@@ -1,9 +0,0 @@
-DEFAULT REL
-section .text
-global collect
-
-collect:
-  ;TODO: generational garbage collection
-  mov rax, 0x2000001      ; System call number for exit = 1
-  mov rdi, 1              ; Exit failure = 1
-  syscall                 ; Invoke the kernel
